d631066508454ee4b8599e051db0ddd177e8421c
abuyoyo <abuyoyo@users.noreply.github.com>
2024-02-18 04:18:17 +0200

    require vendor/autoload.php + fail gracefully on missing deps

--- iac-login.php
+++ iac-login.php
@@ -5,6 +5,27 @@
  * Author: abuyoyo
  * Version: 0.2
  */
+
+// vendor/autoload
+// Allow all other loaders to fail before auto-loading 
+if (
+	(
+		! class_exists( 'WPHelper\PluginCore' )
+		||
+		! trait_exists( 'WPHelper\Utility\Singleton' )
+	)
+	&&
+	file_exists( __DIR__ . '/vendor/autoload.php' )
+	
+) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+// Fail gracefully
+if ( ! class_exists('WPHelper\PluginCore') || ! trait_exists('WPHelper\Utility\Singleton') ) {
+	return;
+}
+
 require_once 'Login_Page.php';
 
 new WPHelper\PluginCore(__FILE__);
