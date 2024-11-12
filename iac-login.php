9aa05bdcc1b21d7e3edc7dcdeac4c21c0d362c9e
abuyoyo <abuyoyo@users.noreply.github.com>
2024-11-12 02:36:28 +0200

    misc cleanup + notes

--- iac-login.php
+++ iac-login.php
@@ -16,13 +16,16 @@ if (
 	)
 	&&
 	file_exists( __DIR__ . '/vendor/autoload.php' )
-	
 ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
 // Fail gracefully
-if ( ! class_exists('WPHelper\PluginCore') || ! trait_exists('WPHelper\Utility\Singleton') ) {
+if (
+	! class_exists( 'WPHelper\PluginCore' )
+	||
+	! trait_exists( 'WPHelper\Utility\Singleton' )
+) {
 	return;
 }
 
